90deb20b66dd1e3230f72198ded6b391bd9e7754
Foster McLane <fkmclane@gmail.com>
2017-07-23 15:51:37 -0400

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
-from distutils.core import setup
+import os
+import re
 
-from db import name, version
+from setuptools import setup, find_packages
+
+
+name = None
+version = None
+
+
+def find(haystack, *needles):
+    regexes = [(index, re.compile("^{}\s*=\s*'([^']*)'$".format(needle))) for index, needle in enumerate(needles)]
+    values = ['' for needle in needles]
+
+    for line in haystack:
+        if len(regexes) == 0:
+            break
+
+        for rindex, (vindex, regex) in enumerate(regexes):
+            match = regex.match(line)
+            if match:
+                values[vindex] = match.groups()[0]
+                del regexes[rindex]
+                break
+
+    return values
+
+
+with open(os.path.join(os.path.dirname(__file__), 'db', 'db.py'), 'r') as web:
+    name, version = find(web, 'name', 'version')
 
 
 setup(
@@ -11,5 +38,5 @@ setup(
     license='MIT',
     author='Foster McLane',
     author_email='fkmclane@gmail.com',
-    packages=['db'],
+    packages=find_packages(),
 )
